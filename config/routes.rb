1f1de20737129330790bc0c2ef85672b94367d2f
inno <innocent.ongeri@student.moringaschool.com>
2022-10-12 16:02:49 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
+  resources :users
+
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   
+  root to: "static#home"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
