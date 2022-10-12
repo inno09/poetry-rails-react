d89adc3ae595d10f74f2ae070224dbb9b5f2eaf7
inno <innocent.ongeri@student.moringaschool.com>
2022-10-12 15:57:44 +0300

    created session controller

--- config/initializers/session_store.rb
+++
@@ -1,4 +0,0 @@
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_poetry-rails-react', domain: 'poetry-rails-react-json-api'
-  else
-    Rails.application.config.session_store :cookie_store, key: '_poetry-rails-react'
